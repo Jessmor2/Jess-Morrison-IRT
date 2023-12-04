4398903e605796e99d4f31c6c0a017411c7b5868
Jess Morrison <Jessmor1993@gmail.com>
2023-12-04 15:15:26 -0500

    added title content, need bg image

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,29 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import IRTLogo from "../../img/LogoMakr-9eCGAY.png";
 import "../../styles/home.css";
+
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<div className="container" id="titleArea">
+			<div className="row justify-content-lg-center">
+				<div className="col col-lg-2">
+					<p className="pageHeaderLeft"> The West Coasts' only complete Billfish outfitter!
+					</p>
+				</div>
+				<div className="col-lg-8 col-sm-4 d-flex flex-column">
+					<h1 className="mt-3 pb-n5" id="pageTitle">Bill Greenway's</h1><br></br>
+					<img ClassName="pt-n5" id="titleImage" src={IRTLogo} />
+				</div>
+				<div className="col col-lg-2" id="pageHeaderRight">
+					1301 N. Gulf Blvd Indian Rocks Beach, FL 33785<br></br><br></br>
+					727-595-3196<br></br><br></br>
+					Bill@indianrockstackle.com
+				</div>
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
 		</div>
 	);
 };
